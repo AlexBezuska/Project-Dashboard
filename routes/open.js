4b4c7039357e29e09bdcbec87b2248fc24d23b98
Alex Bezuska <abezuska@gmail.com>
2017-01-11 23:25:40 -0500

    Update open.js

--- routes/open.js
+++ routes/open.js
@@ -3,14 +3,17 @@ var router = express.Router();
 var exec = require('child_process').exec;
 
 var editorCommand = 'atom';
+var openCommand = 'open';
 
 router.get('/', function(req, res, next) {
   var path = req.query.path;
   var command = req.query.command;
   if (command === "edit"){
     exec(editorCommand + " " + path);
+  } else if (command === "open") {
+    exec(openCommand + " " + path);
   } else {
-    exec(command + " " + path);
+    console.log("Unrecognized command in your projects template /views/partials/projects.hbs");
   }
 
   res.redirect("/");
